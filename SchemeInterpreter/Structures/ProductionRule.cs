451f04d0215c7b100bbcc5199b2923ac9e9bd13f
Jose Ignacio Gonzalez Cardenas <nacho.gon.car@gmail.com>
2016-10-10 13:14:11 -0500

    Add production rule

---
+++ SchemeInterpreter/Structures/ProductionRule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchemeInterpreter.Structures
+{
+    public class ProductionRule
+    {
+        public ProductionRule(Symbol header, List<Symbol> body)
+        {
+            Header = header;
+            Body = body;
+        }
+
+        public Symbol Header { get; set; }
+        public List<Symbol> Body { get; set; }
+
+        public override string ToString()
+        {
+            var bodyString = "\n";
+            foreach (var symbol in Body)
+            {
+                bodyString += symbol.ToString();
+                bodyString += "\n";
+            }
+            return string.Format("Production rule: Header: {0}, Body: {1}", Header, bodyString);
+        }
+    }
+}
