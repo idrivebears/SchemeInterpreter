22c33b1e6bddade7aa75b0f46b66403924dc248c
devcam <josean.camarena@gmail.com>
2016-10-10 12:48:26 -0500

    added short documentation to GetMember

--- SchemeInterpreter/LexerEngine/LexerGenerator.cs
+++ SchemeInterpreter/LexerEngine/LexerGenerator.cs
@@ -14,13 +14,14 @@ namespace SchemeInterpreter.LexerEngine
 
         private static Tuple<string, string> GetMember(string line)
         {
-            var memberName = new Regex(@"\([^:)]+\)");
-            var memberRegEx = new Regex(@":(.*);");
+            var memberName = new Regex(@"\([^:)]+\)"); //Get Token Name
+            var memberRegEx = new Regex(@":(.*);"); //Get regEx for Token
 
             var memName = memberName.Match(line);
             var memRegEx = memberRegEx.Match(line);
 
             return new Tuple<string, string>(memName.Value, memRegEx.Value.Substring(2, memRegEx.Value.Length-4));
+            //Trim is to remove :" leading caracters, and to ignore "; trailing caracters
         }
 
         public static LexerEngine.Lexer Generate(string filename)
@@ -35,6 +36,8 @@ namespace SchemeInterpreter.LexerEngine
                 foreach (var line in lexContent)
                 {
                     var newMember = GetMember(line);
+                    Console.WriteLine("Added Toke: {0} Rule {1}", newMember.Item1,@newMember.Item2);
+
                     lexer.AddDefinition(new TokenDefinition(newMember.Item1, new Regex(@newMember.Item2)));
                 }
                 return lexer;
