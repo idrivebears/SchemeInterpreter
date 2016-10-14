91a91cf415b16ebd9e8852823b9fdce77798fef7
devcam <josean.camarena@gmail.com>
2016-10-14 16:51:27 -0500

    Finish LL1 acceptor

--- SchemeInterpreter/SyntacticAnalysis/LL1.cs
+++ SchemeInterpreter/SyntacticAnalysis/LL1.cs
@@ -9,22 +9,32 @@ namespace SchemeInterpreter.SyntacticAnalysis
 {
     class LL1
     {
-        private int[,] _table;
-        private readonly Dictionary<Symbol, int> _symbLookup;
-        private Symbol _start;
+        private readonly int[,] _table;
+        private readonly Dictionary<Symbol, int> _terminalLookup;
+        private readonly Dictionary<Symbol, int> _nonTerminalLookUp;
+        private readonly Symbol _start;
+        private readonly Grammar _coreGrammar;
 
         public LL1(Grammar g)
         {
+            //Get core grammar
+            _coreGrammar = g; //consider building new grammar object, not only stealing the pointer
+
             //Get all terminal symbols from Grammar
             var terminals = (g.Symbols.Where(s => s.IsTerminal())).ToArray();
-            _symbLookup = new Dictionary<Symbol, int>();
-            for (var i = 0; i < terminals.Length; i++)
-            {
-                _symbLookup.Add(terminals[i], i);
-            }
-            _symbLookup.Add(new Symbol(Symbol.SymTypes.EOS, "$"),terminals.Length);
+            var nonTerminal = (g.Symbols.Where(s => s.IsNonTerminal())).ToArray();
 
-            _table = new int[terminals.Length,g.ProductionRules.Count];
+            _terminalLookup = new Dictionary<Symbol, int>();
+            _nonTerminalLookUp = new Dictionary<Symbol, int>();
+
+            for (var i = 0; i < terminals.Length; i++)
+                _terminalLookup.Add(terminals[i], i);
+            _terminalLookup.Add(new Symbol(Symbol.SymTypes.EOS, "$"),terminals.Length);
+
+            for (var i = 0; i < nonTerminal.Length;i++)
+                _nonTerminalLookUp.Add(nonTerminal[i], i);
+
+            _table = new int[terminals.Length,nonTerminal.Length];
             
             //Get start production (first head of production)
             _start = g.ProductionRules[0].Header;
@@ -37,40 +47,56 @@ namespace SchemeInterpreter.SyntacticAnalysis
             for (var i = 0; i < g.ProductionRules.Count; i++)
             {
                 var focusFirst = g.GetFirstSet(i);
-                //check for epsilon set
 
                 if (focusFirst.Any(s => s.IsEpsilon()))
                 {
-                    var focusFollow = g.FollowSets[g.ProductionRules[i].Header]; //get follow for head of focus production
+                    var focusFollow = g.FollowSets[g.ProductionRules[i].Header];
                     foreach (var term in focusFollow)
-                    {
-                        //get index
-                        var symIndex = _symbLookup[term];
-                        //place production rule id in table
-                        _table[symIndex, i] = i;
-                    }
+                        _table[_terminalLookup[term], _nonTerminalLookUp[g.ProductionRules[i].Header]] = i+1;
                 }
                 
                 foreach (var term in focusFirst)
-                {
-                    //get index
-                    var symIndex = _symbLookup[term];
-                    //place production rule id in table
-                    _table[symIndex, i] = i;
-                }
+                    _table[_terminalLookup[term], _nonTerminalLookUp[g.ProductionRules[i].Header]] = i+1;
             }
         }
 
         public bool Accept(string input)
         {
-            var sym = new Stack<Symbol>();
-            sym.Push(new Symbol(Symbol.SymTypes.EOS, "$"));
-            sym.Push(_start); //initialize eval stack
+            var symStack = new Stack<Symbol>();
+            var inputQueue = new Queue<Symbol>();
 
-            //ToDo Acceptance algorithm
+            symStack.Push(new Symbol(Symbol.SymTypes.EOS, "$"));
+            symStack.Push(_start); //initialize eval stack
 
+            foreach (var c in input)
+                inputQueue.Enqueue(new Symbol(Symbol.SymTypes.Terminal, c.ToString())); 
+            inputQueue.Enqueue(new Symbol(Symbol.SymTypes.EOS, "$")); //initialize input queue
 
-            return false;
+            while (symStack.Count != 0 && inputQueue.Count != 0)
+            {
+                var stackSymbol = symStack.Pop();
+                var inputSymbol = inputQueue.Peek();
+
+                //check for collapsing conditions
+                if (Equals(stackSymbol, inputSymbol))
+                    inputQueue.Dequeue(); //remove symbol from input
+                else if (stackSymbol.IsTerminal() && inputSymbol.IsTerminal() && !Equals(stackSymbol, inputSymbol))
+                    return false; //symbols are terminal, but they do not match, reject string
+                else
+                {
+                    var nextProductionId = _table[_terminalLookup[inputSymbol], _nonTerminalLookUp[stackSymbol]];
+                    //check for undefined productions
+                    if (nextProductionId == 0)
+                        return false; //transition is not defined for given pair
+                    var nextProduction = _coreGrammar.ProductionRules[nextProductionId-1];
+                    //push body to stack
+                    if(nextProduction.Body.First().IsTerminal())
+                        continue;
+                    foreach (var sym in nextProduction.Body)
+                        symStack.Push(sym);
+                }
+            }
+            return symStack.Count == 0 && symStack.Count == inputQueue.Count; //accept if empty stack and queue, reject otherwise
         }
     }
 }
--- SchemeInterpreterTest/SyntacticAnalysis/LL1Test.cs
+++ SchemeInterpreterTest/SyntacticAnalysis/LL1Test.cs
@@ -1,15 +1,68 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SchemeInterpreter.Structures;
 
 namespace SchemeInterpreterTest.SyntacticAnalysis
 {
     [TestClass]
     public class LL1Test
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
+        private Grammar g;
+        private Dictionary<string, Symbol> symbols;
 
+        public void LL1TableTest()
+        {
+            symbols = new Dictionary<string, Symbol>
+            {
+                {"S", new Symbol(Symbol.SymTypes.NoTerminal, "S")},
+                {"P", new Symbol(Symbol.SymTypes.NoTerminal, "P")},
+                {"M", new Symbol(Symbol.SymTypes.NoTerminal, "M")},
+                {"E", new Symbol(Symbol.SymTypes.NoTerminal, "E")},
+                {"K", new Symbol(Symbol.SymTypes.NoTerminal, "K")},
+                {"q", new Symbol(Symbol.SymTypes.Terminal, "q")},
+                {"n", new Symbol(Symbol.SymTypes.Terminal, "n")},
+                {"t", new Symbol(Symbol.SymTypes.Terminal, "t")},
+                {"r", new Symbol(Symbol.SymTypes.Terminal, "r")},
+                {"u", new Symbol(Symbol.SymTypes.Terminal, "u")},
+            };
+
+            var productionRules = new List<ProductionRule>();
+
+            var productionRuleBody = new List<Symbol> { symbols["P"], symbols["M"] };
+            var productionRule = new ProductionRule(symbols["S"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["E"] };
+            productionRule = new ProductionRule(symbols["P"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["q"] };
+            productionRule = new ProductionRule(symbols["P"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["n"], symbols["E"]};
+            productionRule = new ProductionRule(symbols["M"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["t"] };
+            productionRule = new ProductionRule(symbols["M"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["r"] , symbols["K"] };
+            productionRule = new ProductionRule(symbols["E"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["M"] };
+            productionRule = new ProductionRule(symbols["E"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            productionRuleBody = new List<Symbol> { symbols["u"] , symbols["K"]};
+            productionRule = new ProductionRule(symbols["K"], productionRuleBody);
+            productionRules.Add(productionRule);
+
+            g = new Grammar(productionRules, symbols.Values.ToList());
         }
     }
 }
