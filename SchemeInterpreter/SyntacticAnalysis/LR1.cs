bf737e03c91f82e50a732d309821e0904de7ac16
Alex Walls <idrivebears@gmail.com>
2016-10-28 14:57:54 -0500

    Added acceptance state

--- SchemeInterpreter/SyntacticAnalysis/LR1.cs
+++ SchemeInterpreter/SyntacticAnalysis/LR1.cs
@@ -11,8 +11,6 @@ namespace SchemeInterpreter.SyntacticAnalysis
     //Left at: create new state into automata
 
         /*
-         Todo add epsilon
-         Add reduce rule marking
          Add eos
          */
     public class LR1
@@ -129,6 +127,11 @@ namespace SchemeInterpreter.SyntacticAnalysis
 
                 state.Value.Explored = true;
             }
+
+            var accSym = new Symbol(Symbol.SymTypes.EOS, "$");
+
+            var acceptanceState = new LR1AutomataState(_automata.Keys.Count, new ProductionRule(accSym, new List<Symbol>()));
+            _automata[firstState.Header].KernelTransitions.Add(new Symbol(Symbol.SymTypes.NoTerminal, "S"), acceptanceState.Header);
         }
 
         private void GenerateAutomataStates()
